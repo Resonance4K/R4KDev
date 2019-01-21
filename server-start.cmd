876e363c0c0a1baf5f979ca7ff893f145a1e2151
Resonance4K <Resonance4K@gmail.com>
2019-01-22 00:12:01 +0800

    Fixed syntax and formatting

--- server-start.cmd
+++ server-start.cmd
@@ -1,7 +1,7 @@
 
 @echo off
 
-@echo\
+echo\
 echo Running the server-start.cmd file for R4KDev server execution.
 echo\
 echo ----------------------------------------------------------------------
@@ -10,8 +10,7 @@ echo\
 node server.js
 
 echo\
-echo --------------------------------------------------
-
+echo ----------------------------------------------------------------------
 echo\
 pause > nul | set /p = Press any key to exit...
 exit
