ef09d435283bf8a1894fba5d54f720a764971568
Resonance4K <Resonance4K@gmail.com>
2020-04-27 16:04:49 +0800

    Added a server start script for Linux

---
+++ server-start.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+echo
+echo "Running the server-start.sh file for R4KDev server execution."
+echo
+echo "----------------------------------------------------------------------"
+echo
+
+node server.js
+
+echo
+echo "----------------------------------------------------------------------"
+echo
+read -s -p "Press the enter key to exit..." key
+echo
+echo
+exit
