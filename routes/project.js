6b0711b6b59ba1b5b0df2e4483d393a7f96ecd14
Resonance4K <Resonance4K@gmail.com>
2020-08-03 23:16:27 +0800

    Implemented SFR project page

--- routes/project.js
+++ routes/project.js
@@ -6,5 +6,25 @@ module.exports.getRoutes = function getRoutes()
 {
 	const routes = [];
 
+	routes.push(
+	{
+		method: "GET",
+		path: "/projects/dev/SFR",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, "/html/projects/dev/sfr.html", MIME_TYPE.HTML);
+		}
+	});
+
+	routes.push(
+	{
+		method: "GET",
+		path: "/images/projects/dev/sfr-1.png",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, this.path, MIME_TYPE.PNG);
+		}
+	});
+
 	return routes;
 }
