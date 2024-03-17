2b23b023e44729956bff1c4b5d53a505dfa11cce
Resonance4K <Resonance4K@gmail.com>
2024-03-17 15:44:31 +0800

    Add gaming project related routing to the server routes file

--- routes/project.js
+++ routes/project.js
@@ -66,5 +66,105 @@ module.exports.getRoutes = function getRoutes()
 		}
 	});
 
+	routes.push(
+	{
+		method: "GET",
+		path: "/projects/gaming/simple2d/S2DEngine",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, "/html/projects/gaming/simple2d/s2dengine.html", MIME_TYPE.HTML);
+		}
+	});
+
+	routes.push(
+	{
+		method: "GET",
+		path: "/images/projects/gaming/simple2d/s2dengine-1.png",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, this.path, MIME_TYPE.PNG);
+		}
+	});
+
+	routes.push(
+	{
+		method: "GET",
+		path: "/projects/gaming/simple2d/S2DTileMap",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, "/html/projects/gaming/simple2d/s2dtilemap.html", MIME_TYPE.HTML);
+		}
+	});
+
+	routes.push(
+	{
+		method: "GET",
+		path: "/images/projects/gaming/simple2d/s2dtilemap-1.png",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, this.path, MIME_TYPE.PNG);
+		}
+	});
+
+	routes.push(
+	{
+		method: "GET",
+		path: "/projects/gaming/simple2d/S2DToolkit",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, "/html/projects/gaming/simple2d/s2dtoolkit.html", MIME_TYPE.HTML);
+		}
+	});
+
+	routes.push(
+	{
+		method: "GET",
+		path: "/images/projects/gaming/simple2d/s2dtoolkit-1.png",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, this.path, MIME_TYPE.PNG);
+		}
+	});
+
+	routes.push(
+	{
+		method: "GET",
+		path: "/projects/gaming/quake3/OpposingForce",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, "/html/projects/gaming/quake3/opposing-force.html", MIME_TYPE.HTML);
+		}
+	});
+
+	routes.push(
+	{
+		method: "GET",
+		path: "/images/projects/gaming/quake3/opposing-force-1.png",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, this.path, MIME_TYPE.PNG);
+		}
+	});
+
+	routes.push(
+	{
+		method: "GET",
+		path: "/projects/gaming/quake3/eNautica",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, "/html/projects/gaming/quake3/enautica.html", MIME_TYPE.HTML);
+		}
+	});
+
+	routes.push(
+	{
+		method: "GET",
+		path: "/images/projects/gaming/quake3/enautica-1.png",
+		action: function(request, response)
+		{
+			APPLICATION.getResource(response, this.path, MIME_TYPE.PNG);
+		}
+	});
+
 	return routes;
 }
